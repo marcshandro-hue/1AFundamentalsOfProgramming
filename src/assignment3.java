da41d1e8d1748e80e722257c88c3afde192ced91
marcshandro-hue <marcshandro@gmail.com>
2026-09-01 17:50:32 +0800

    Initial Commit for this project

---
+++ src/assignment3.java
@@ -0,0 +1,20 @@
+public class assignment3
+{
+    public static void main(String[] args)
+    {
+
+        String firstname = "Marc Shandro";
+        String lastname = "Santiago";
+        String address = "Asingan";
+        String mobileNumber = "09554520666";
+        int age = 18;
+        double gwa = 98.11;
+
+        System.out.println("Firstname: " + firstname);
+        System.out.println("Lastname: " + lastname);
+        System.out.println("Address: " + address);
+        System.out.println("Mobile Number: " + mobileNumber);
+        System.out.println("Age: " + age);
+        System.out.println("GWA: " + gwa);
+    }
+}
